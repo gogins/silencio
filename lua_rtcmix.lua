7855805f57658c176eebb733f4dd82dddf8a76d5
michael.gogins@gmail.com <michael.gogins@gmail.com@807b12c0-8905-ba0d-94dd-99ea2fd2debc>
2012-01-12 03:13:12 +0000

    Changed to FFI stubs in RTcmix.cpp.

--- lua_rtcmix.lua
+++ lua_rtcmix.lua
@@ -1,19 +1,41 @@
 print('Hello, this is RTcmix from Lua.')
+
 local jit = require('jit')
 local ffi = require('ffi')
 local cmix = ffi.load('/home/mkg/RTcmix/lib/librtcmix.so')
--- Declare mangled C++ functions so they can be called as C.
--- They all seem to be static, and the RTcmix constructors don't do anything but 
--- call them, so that makes it easier.
+print('cmix:', cmix)
 ffi.cdef[[
-//	static void RTcmix::init_globals(bool fromMain, const char *defaultDSOPath);
-void _ZN6RTcmix12init_globalsEbPKc(bool fromMain, const char *defaultDSOPath);
-//	void RTcmix::init(float, int, int, const char*, const char*, const char*);	// called by all constructors
-void _ZN6RTcmix4initEfiiPKcS1_S1_(int, float, int, int, const char*, const char*, const char*);
+void ff_create();
+void ff_create2(double tsr, int tnchans);
+void ff_create6(double tsr, int tnchans, int bsize, const char *opt1, const char *opt2, const char *opt3);
+double ff_cmdval(const char *name);
+double ff_cmdval_d(const char *name, int n_args, double p0, ...);
+double ff_cmdval_s(const char *name, int n_args, const char* p0, ...);
+void *ff_cmd_d(const char *name, int n_args, double p0, ...);
+void *ff_cmd_s(const char *name, int n_args, const char* p0, ...);
+void ff_printOn();
+void ff_printOff();
+void ff_run();
+void ff_close();
+void ff_destroy();
+unsigned int sleep(unsigned int seconds);
 ]]
-cmix._ZN6RTcmix12init_globalsEbPKc(false, "/home/mkg/RTcmix/shlib")
--- Pass 0 as 'this' pointer which is not actually used (otherwise, CRASH!).
-cmix._ZN6RTcmix4initEfiiPKcS1_S1_(0, 44100, 2, 0, 'device = plughw:1', none, none)
+
+cmix.ff_create6(44100, 2, 128, "device=hw:1", nil, nil)
+ffi.C.sleep(1)
+cmix.ff_printOn()
+cmix.ff_cmd_s("load", 1, "STRUM")
+cmix.ff_cmd_d("makegen", 7, 1.0, 24.0, 1000.0, 0.0, 1.0, 1.0, 1.0);
+cmix.ff_cmd_d("makegen", 11, 2.0, 24.0, 1000.0, 0.0, 0.0, 0.05, 1.0, 0.95, 1.0, 1.0, 0.0);
+cmix.ff_cmd_d("STRUM", 7, 0.0, 1.0, 0.1, 106.0, 25.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 1.0, 1.0, 0.1, 95.0, 21.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 2.0, 1.0, 0.1, 89.0, 19.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 3.0, 1.0, 0.1, 75.0, 19.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 4.0, 1.0, 0.1, 70.0, 15.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 5.0, 1.0, 0.1, 67.0, 16.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 6.0, 1.0, 0.1, 56.0, 17.0, 5000.0, 0.5);
+cmix.ff_cmd_d("STRUM", 7, 7.0, 1.0, 0.1, 53.0, 25.0, 5000.0, 0.5);
+ffi.C.sleep(8)
 print('Done.')
 
 
