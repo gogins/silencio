faae2a9b7315544fde469f265b919917a53cf30f
michael.gogins@gmail.com <michael.gogins@gmail.com@807b12c0-8905-ba0d-94dd-99ea2fd2debc>
2012-01-28 03:26:22 +0000

    Code working but no sound...

--- trunk/ffi_rtcmix.lua
+++ trunk/ffi_rtcmix.lua
@@ -98,11 +98,11 @@ The double brackets enclose Lua multi-line string constants;
 zero or more equals signs within the brackets denote zero or 
 more levels of nesting.
 ]]
-cmix.ffi_cmd_s("lua_intro", 2, "LUA_OSC", [=[
+cmix.ffi_cmd_s("lua_intro", 2, "LUA_OSC", [[
 local ffi = require('ffi')
 local math = require('math')
 local m = ffi.load('m')
-ffi.cdef[[
+ffi.cdef[=[
   double sin(double);
   void *ffi_create(double tsr, int tnchans, int bsize, const char *opt1, const char *opt2, const char *opt3);
   double ffi_cmdval(const char *name);
@@ -121,6 +121,16 @@ ffi.cdef[[
   void ffi_destroy();
   int LUA_INTRO(const char *NAME, const char *luacode);
   int advise(const char*, const char *,...);
+  double cpsoct(double oct);
+  double octcps(double cps);
+  double octpch(double pch);
+  double cpspch(double pch);
+  double pchoct(double oct);
+  double pchcps(double cps);
+  double midipch(double pch);
+  double pchmidi(unsigned char midinote);
+  double octmidi(unsigned char midinote);
+  void *calloc(size_t num, size_t size);	
   struct LuaInstrumentState
   {
     char *name;
@@ -143,7 +153,7 @@ ffi.cdef[[
     // state, and set this pointer to the address of that structure.
     void *instanceState;
   };
-]]
+]=]
 
 -- Obtain a ctype for a pointer to the LuaInstrumentState struct,
 -- for greater efficiency.
@@ -175,8 +185,7 @@ function LUA_OSC_run(state)
 	 return 0
 end
 
-print('End of Lua code being registered.')
-]=])
+]])
 
 -- The ffi_cmd_l function invokes a Lua instrument; there is a second
 -- string name for the actual Lua instrument class defined above.
@@ -195,13 +204,11 @@ Chua's oscillator written in Lua!...
 
 ]]
 
-cmix.ffi_cmd_s("lua_intro", 2, "CHUA", [=[
+cmix.ffi_cmd_s("lua_intro", 2, "CHUA", [[
 local ffi = require('ffi')
 local math = require('math')
 local m = ffi.load('m')
-ffi.cdef[[
-
-  // Define the instance state for Chua's oscillator.
+ffi.cdef[=[
 
   struct CHUA 
   {
@@ -214,11 +221,14 @@ ffi.cdef[[
 	/* State variables */
 	double Vc1;
 	double Vc2;
-	double Il;		
+	double I1;		
 	/* Derivatives of state variables */
 	double dVc1;
 	double dVc2;
-	double dIl;
+	double dI1;
+	double L;
+	double C1;
+	double C2;
 	/* Time scaled values of L,C1 and C2 */
 	double Ls;
 	double C1s;
@@ -226,89 +236,98 @@ ffi.cdef[[
 	/* Used for time scaling */
 	double tscale;			
 	/* Frequency of fundamental harmonic */
-	double frequency;		
-	/* Time increment of each differiantial step */
+	double freq;		
+	/* Time increment of each differential step */
 	double tstep;			
-	double G;
 	double G1;
-	double g2;
+	double G2;
+	double waitc;
   };
-]]
+]=]
 
 local LuaInstrumentState_ct = ffi.typeof("struct LuaInstrumentState *")
-local CHUA_ct = ffi.typeof("struct CHUA");
-local CHUAp_ct = ffi.typeof("struct CHUA *");
-
--- We may, if we wish, load RTcmix into the symbol table for the Lua instrument.
-
+local CHUA_ct = ffi.typeof("struct CHUA")
+local CHUAp_ct = ffi.typeof("struct CHUA *")
 local cmix = ffi.load('/home/mkg/RTcmix/lib/librtcmix.so', true)
 
 function CHUA_init(state)
-	-- Type casting the ctype for LuaInstrumentState enables us to 
-	-- access members of that type as though they are Lua variables.
 	local luastate = ffi.cast(LuaInstrumentState_ct, state)
-	local chua = CHUA_ct()
+	luastate.instanceState = ffi.C.calloc(1, ffi.sizeof(CHUA_ct))
+	local chua = ffi.cast(CHUAp_ct, luastate.instanceState)
 	cmix.advise('CHUA', '%s', tostring(chua))
-	-- SNEAKY, SEAKY (part I)!! LuaJIT assigns struct as a pointer to struct (which automatically casts to void *).
-	luastate.instanceState = chua
 	cmix.advise('CHUA', 'p[ 1] outskip: %9.4f', luastate.parameters[ 1])
 	cmix.advise('CHUA', 'p[ 2] inskip:  %9.4f', luastate.parameters[ 2])
 	cmix.advise('CHUA', 'p[ 3] dur:     %9.4f', luastate.parameters[ 3])
 	cmix.advise('CHUA', 'p[ 4] amp:     %9.4f', luastate.parameters[ 4])
-	cmix.advise('CHUA', 'p[ 5] pitch:   %9.4f', luastate.parameters[ 5]) 
-	cmix.advise('CHUA', 'p[ 6] L:       %9.4f', luastate.parameters[ 6]) 
-	cmix.advise('CHUA', 'p[ 7] C1:      %9.4f', luastate.parameters[ 7]) 
-	cmix.advise('CHUA', 'p[ 8] C2:      %9.4f', luastate.parameters[ 8]) 
-	cmix.advise('CHUA', 'p[ 9] G:       %9.4f', luastate.parameters[ 9]) 
-	cmix.advise('CHUA', 'p[10] Bp1:     %9.4f', luastate.parameters[10]) 
-	cmix.advise('CHUA', 'p[11] Bp2:     %9.4f', luastate.parameters[11]) 
-	cmix.advise('CHUA', 'p[12] m0:      %9.4f', luastate.parameters[12]) 
-	cmix.advise('CHUA', 'p[13] m1:      %9.4f', luastate.parameters[13]) 
-	cmix.advise('CHUA', 'p[14] m2:      %9.4f', luastate.parameters[14]) 
+	chua.freq = 	    	   	    	    cmix.cpsoct(cmix.octmidi(luastate.parameters[ 5]))
+	cmix.advise('CHUA', 'p[ 5] freq :   %9.4f', chua.freq) 
+	chua.L     = 	    	   	    	    luastate.parameters[ 6]
+	cmix.advise('CHUA', 'p[ 6] L:       %9.4f', chua.L) 
+	chua.C1    = 	    	   	    	    luastate.parameters[ 7]
+	cmix.advise('CHUA', 'p[ 7] C1:      %9.4f', chua.C1) 
+	chua.C2    = 	    	   	    	    luastate.parameters[ 8]
+	cmix.advise('CHUA', 'p[ 8] C2:      %9.4f', chua.C2) 
+	chua.G1    = 	    	   	    	    luastate.parameters[ 9]
+	cmix.advise('CHUA', 'p[ 9] G:       %9.4f', chua.G1) 
+	chua.Bp1   = 	    	   	    	    luastate.parameters[10]
+	cmix.advise('CHUA', 'p[10] Bp1:     %9.4f', chua.Bp1) 
+	chua.Bp2   = 	    	   	    	    luastate.parameters[11]
+	cmix.advise('CHUA', 'p[11] Bp2:     %9.4f', chua.Bp2) 
+	chua.m0    = 	    	   	    	    luastate.parameters[12]
+	cmix.advise('CHUA', 'p[12] m0:      %9.4f', chua.m0) 
+	chua.m1    = 	    	   	    	    luastate.parameters[13]
+	cmix.advise('CHUA', 'p[13] m1:      %9.4f', chua.m1) 
+	chua.m2 = 	    	   	    	    luastate.parameters[14]
+	cmix.advise('CHUA', 'p[14] m2:      %9.4f', chua.m2)
+	chua.tscale = 0.325 / chua.freq
+	chua.waitc = 20
+	chua.tstep = 1.0 / chua.waitc * 1 / cmix.ffi_sr()
+	chua.Vc1 = 0
+	chua.Vc2 = 0.1
+	chua.I1 = 0
+	chua.Ls = chua.tscale * chua.L
+	chua.C1s = chua.tscale * chua.C1
+	chua.C2s = chua.tscale * chua.C2
 	return 0
 end
 
 function CHUA_run(state)
-	-- Type casting the ctype for a type enables us to 
-	-- access members of that type as though they are Lua variables.
 	local luastate = ffi.cast(LuaInstrumentState_ct, state)
-	-- SNEAKY, SEAKY (part II)!! LuaJIT dereferences POINTER to struct with same semantics as struct.
 	local chua = ffi.cast(CHUAp_ct, luastate.instanceState)
-	print(tostring(chua), chua.Vc2)
 	if chua.Vc1 < chua.Bp1 then
-	   chua.g2 = chua.m0 * (chua.Vc1 - chua.Bp1) + chua.m1 * chua.Bp1
+	   chua.G2 = chua.m0 * (chua.Vc1 - chua.Bp1) + chua.m1 * chua.Bp1
 	end
 	if (chua.Vc1 >= chua.Bp1) and (chua.Vc1 <= chua.Bp2) then 
-	   chua.g2 = chua.m1 * chua.Vc1
+	   chua.G2 = chua.m1 * chua.Vc1
 	end
 	if chua.Vc1 > chua.Bp2 then
-	   chua.g2 = chua.m2 * (chua.Vc1 - chua.Bp2) + chua.m1 * chua.Bp2
+	   chua.G2 = chua.m2 * (chua.Vc1 - chua.Bp2) + chua.m1 * chua.Bp2
 	end
-	chua.dVc1 = (chua.G1 * (chua.Vc2 - chua.Vc1) - chua.g2) / chua.C1s --L70
-	chua.dVc2 = (chua.G1 * (chua.Vc1 - chua.Vc2) + chua.Il) / chua.C2s
-	chua.dIl = - (chua.Vc2 / chua.Ls)
-	chua.Vc1 = chua.Vc1 + (chua.dVc1 * chua.tstep)
-	chua.Vc2 = chua.Vc2 + (chua.dVc2 * chua.tstep)
-	chua.Il = chua.Il + (chua.dIl * chua.tstep)
-	luastate.output[0] = chua.Il
-	luastate.output[1] = chua.Il
-	print(tostring(chua), chua.Vc2)
-	os.exit()
+	chua.dVc1 = (chua.G1 * (chua.Vc2 - chua.Vc1) - chua.G2) / chua.C1s 
+	chua.dVc2 = (chua.G1 * (chua.Vc1 - chua.Vc2) + chua.I1) / chua.C2s
+	chua.dI1 = - chua.Vc2 / chua.Ls
+	chua.Vc1 = chua.Vc1 + chua.dVc1 * chua.tstep
+	chua.Vc2 = chua.Vc2 + chua.dVc2 * chua.tstep
+	chua.I1 = chua.I1 + chua.dI1 * chua.tstep
+	luastate.output[0] = chua.Vc1 * luastate.parameters[4]
+	luastate.output[1] = chua.Vc1 * luastate.parameters[4]
+	--print (luastate.output[0])
 	return 0
 end
 
-print('End of Lua code being registered.')
-]=])
+]])
 
-cmix.ffi_cmd_l("LUAINST", "CHUA", 14, 18.0, 0.0, 20.0, 4000.0, 48.0, 0.142857, 0.111111, 1, 0.7, -1, 1, -0.5, -0.8, -0.5)
+cmix.ffi_cmd_l("LUAINST", "CHUA", 14, 16.0, 0.0, 120.0, 4000.0, 48.0, 0.142857, 0.111111, 1, 0.7, -1, 1, -0.5, -0.8, -0.5)
 
--- Give the RTcmix performance thread time to do its work.
+print[[
 
-print('Press Control-C to exit....')
+Press Control-C to exit....'
+
+]]
+
 while true do
       ffi.C.sleep(1)
 end
-print()
 
 
 
